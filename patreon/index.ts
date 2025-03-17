71cdbb81a6ae5ff8a2909a2406cea51c36eb8b1f
Carl Vitullo <carl.vitullo@gmail.com>
2025-03-17 16:35:35 -0400

    Fix edge cases in patreon detection
    - oops, vetted members were just broken
    - early return if roles didn't change
    - fetch full member details if partial record is loaded

--- patreon/index.ts
+++ patreon/index.ts
@@ -6,6 +6,7 @@ export const client = new Client({
 });
 
 const privateRole = "1260328377940836462";
+const vettedRole = "1260328524087033896";
 const subscriberRole = "1334572997687771196";
 const accessRole = "1336774844292923533";
 
@@ -25,26 +26,44 @@ client
     process.exit(1);
   });
 
-client.on("guildMemberUpdate", async (_, newMember) => {
+client.on("guildMemberUpdate", async (oldMember, newMember) => {
+  if (newMember.partial) {
+    newMember = await newMember.fetch();
+  }
+  if (oldMember.partial) {
+    oldMember = await oldMember.fetch();
+  }
   console.log(
     "guildmemberupdate",
-    `old: ${_.roles.cache.size}. new: ${
+    `old: ${oldMember.roles.cache.size}. new: ${
       newMember.roles.cache.size
     } (${newMember.roles.cache.map((r) => r.name).join(",")})`,
   );
+  if (
+    oldMember.roles.cache.size === newMember.roles.cache.size &&
+    oldMember.roles.cache.every((r) => newMember.roles.cache.has(r.id))
+  ) {
+    console.log("guildmemberupdate", "roles did not change");
+    return;
+  }
+
   if (newMember.roles.cache.has(accessRole)) {
     // If they have the access role but not private+patreon, remove the access role
-    if (!newMember.roles.cache.hasAll(privateRole, subscriberRole)) {
+    if (
+      !newMember.roles.cache.hasAll(privateRole, subscriberRole) &&
+      !newMember.roles.cache.hasAll(vettedRole, subscriberRole)
+    ) {
       console.log(
         `User ${newMember.id} no longer has private+patreon, removing access`,
       );
       await newMember.roles.remove(accessRole);
     }
-  } else {
-    if (newMember.roles.cache.hasAll(privateRole, subscriberRole)) {
-      console.log(`User ${newMember.id} now has access to subscriber channels`);
-      await newMember.roles.add(accessRole);
-    }
+  } else if (
+    newMember.roles.cache.hasAll(privateRole, subscriberRole) ||
+    newMember.roles.cache.hasAll(vettedRole, subscriberRole)
+  ) {
+    console.log(`User ${newMember.id} now has access to subscriber channels`);
+    await newMember.roles.add(accessRole);
   }
 });
 
