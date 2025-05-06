5251ba75bc326a4c34c8054b2cdce63dca52f1cd
Carl Vitullo <carl.vitullo@gmail.com>
2025-05-06 18:24:19 -0400

    Better event rendering

--- web/src/features/calendar/Calendar.tsx
+++ web/src/features/calendar/Calendar.tsx
@@ -11,7 +11,7 @@ export const Calendar = ({ events }: { events: Event[] }) => {
       return {
         id: e.id,
         date: e.hide_end_date ? undefined : e.start.utc,
-        title: e.name.text,
+        title: `${e.name.text}|||${e.description.text}|||${e.logo.url}`,
         url: e.url,
       };
     });
@@ -21,9 +21,29 @@ export const Calendar = ({ events }: { events: Event[] }) => {
     <FullCalendar
       ref={calendarRef}
       plugins={[dayGridPlugin]}
-      initialView="dayGridWeek"
-      eventDisplay="block"
+      initialView="dayGridMonth"
       events={renderedEvents}
+      eventContent={(eventInfo) => {
+        const [title, description, image] = eventInfo.event.title.split("|||");
+        const [tag, name] = title.split(": ");
+        return (
+          <div className="overflow-hidden bg-purple-400 text-gray-900 p-1">
+            <div className="w-fit">
+              <div>
+                <span>{eventInfo.timeText}</span> •{" "}
+                {tag ? <span>{tag}</span> : undefined}
+              </div>
+              <div>{tag ? <span>{name}</span> : <span>{title}</span>}</div>
+            </div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              className="pointer-events-none"
+              alt={description}
+              src={image}
+            />
+          </div>
+        );
+      }}
     />
   );
 };
